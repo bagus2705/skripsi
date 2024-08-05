c43c715791c817fc688498634af6b50f55fcb7e5
bagus2705 <bagusprasetyo2705@gmail.com>
2024-08-06 05:57:14 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                "resources/css/dashboard.css",
+                "resources/css/landing.css",
+                "resources/css/navbar.css",
+            ,'resources/css/style.css'],
             refresh: true,
         }),
     ],
